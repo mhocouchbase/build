7ac1079c1c7bc279e893730f78ed49ebc2bc0167
Ming Ho <ming.ho@couchbase.com>
2021-01-18 10:28:38 -0800

    merged missing variable from master.
    -Ming Ho

--- scripts/codesign/codesignApple.sh
+++ scripts/codesign/codesignApple.sh
@@ -33,6 +33,10 @@ OSX=${4} # macos vs elcapitan
 
 DOWNLOAD_NEW_PKG=${5}  # Get new build
 
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" #find out absolute path of the script
+
+ARCHITECTURE='x86_64'
+
 result="rejected"
 
 PKG_URL=http://latestbuilds.service.couchbase.com/builds/latestbuilds/${PRODUCT}/zz-versions/${PKG_VERSION}/${PKG_BUILD_NUM}
