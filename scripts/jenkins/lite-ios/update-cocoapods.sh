be1336af7001f6e9ea8aaf653bb5c9ea1c6f48e7
Ming Ho <ming.ho@couchbase.com>
2021-02-26 15:19:07 -0800

    CBD-3560, modify build scripts so that they can run on ubuntu20.04 based zz-lightweight * fixed rvm & gem * convert python2 to python3
    -Ming Ho

--- scripts/jenkins/lite-ios/update-cocoapods.sh
+++ scripts/jenkins/lite-ios/update-cocoapods.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+#Pickup newer ruby which was installed via rvm
+source /etc/profile.d/rvm.sh
+
+# Install required cocoapods
+cocoapods_version=1.9.3
+rvmsudo gem uninstall --all cocoapods
+rvmsudo gem install cocoapods -v ${cocoapods_version}
+mkdir $HOME/.gem
+export GEM_HOME=$HOME/.gem
+gem which cocoapods
+
 # virtualenv stuff
 export LC_ALL=C
 export LANG=en_US.UTF-8
-
-#pickup newer ruby which was installed via rvm
-source /etc/profile.d/rvm.sh
 
 pip3 install --user --upgrade virtualenv
 
@@ -13,14 +21,10 @@ pip3 install --user --upgrade virtualenv
 /home/couchbase/.local/bin/virtualenv COCOAPODS || exit 1
 source ./COCOAPODS/bin/activate  || exit 1
 
-# Install required cocoapods
-sudo gem -E uninstall --all cocoapods
-sudo gem -E install cocoapods -v 1.8.4
-export GEM_HOME=$HOME/.gem
-gem which cocoapods
-
 # Checkout required repo
 cd ${WORKSPACE} && git clone git@github.com:couchbaselabs/couchbase-lite-ios-ee.git couchbase-lite-ios-ee
+cd couchbase-lite-ios-ee
+git checkout ${BRANCH}
 
 cd ${WORKSPACE}/couchbase-lite-ios-ee/Podspecs
 declare -a JSON_FILES=( $(ls) )
